d67cc92d6044822930bc7849bdf01ec2405d4749
A. F <hello@clivern.com>
2019-05-04 15:13:54 +0200

    Update catalog.py

--- pymako/catalog.py
+++ pymako/catalog.py
@@ -7,5 +7,5 @@ from .client import Client
 
 class Catalog(Client):
 
-    def __init__(self):
-        super().__init__()
+    def __init__(self, consul_url, api_version="v1"):
+        super().__init__(consul_url, api_version)
